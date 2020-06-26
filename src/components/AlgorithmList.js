29ad78caad7febed82321419689eb42699e22aab
gitburd <denaburd@gmail.com>
2020-06-26 12:25:44 -0700

    add sort

--- src/components/AlgorithmList.js
+++ src/components/AlgorithmList.js
@@ -1,20 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Data from '../data/data'
 import AlgorithmItem from './AlgorithmItem'
+import Sort from './Sort'
 
-const AlgorithmList = () => {
-    console.log(Data)
-    return (
-        <div>
-            <h1>Algorithm List</h1>
-            {Data && Data.length>0 && (
-                Data.map((algorithm, idx) => (
-                    <AlgorithmItem algorithm={algorithm} key={idx}/>
-                ) )
-            )}
+class AlgorithmList extends Component{
 
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+          selectedSort: "default",
+          displayData:[]
+        };
+      }
+    
+    componentDidMount(){
+        this.sortData('default')
+    }
+
+    handleSortChange = (selectedSort) => {
+        this.setState({ selectedSort })
+        this.sortData(selectedSort)
+    } 
+
+    
+    sortData(sort){
+        if(sort === 'default'){
+            let display = []
+            Data.forEach((el)=> display.push(el))
+            this.setState({displayData:display})
+        }
+        if(sort === 'az'){
+            let sortedData = 
+            this.state.displayData.sort(
+                (a, b) => a.name.localeCompare(b.name)
+            )
+            this.setState({displayData:sortedData})
+        }
+        if(sort === 'created'){
+            console.log('created!')
+            let sortedData = 
+            this.state.displayData.sort(
+                (a, b) => {
+                    return new Date(b.addedOn) - new Date(a.addedOn);
+                }
+            )
+            this.setState({displayData:sortedData})
+        }
+    }
+    
+    render(){
+        const {displayData} = this.state
+
+        return (
+            <div>
+                <Sort selectedSort={this.state.selectedSort} handleSortChange={this.handleSortChange} />
+                <h1>Algorithm List</h1>
+                {displayData && displayData.length>0 && (
+                    displayData.map((algorithm, idx) => (
+                        <AlgorithmItem algorithm={algorithm} key={idx}/>
+                    ) )
+                )}
+
+            </div>
+        )
+    }
 }
 
 export default AlgorithmList
---
+++ src/components/Sort.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Sort = ({handleSortChange, selectedSort}) => {
+    
+    const handleChange = (e) => {
+        handleSortChange(e.target.value)
+    }
+
+    return (
+        <div>
+            <form>
+                <div className="form-check">
+                <label>
+                    <input
+                    type="radio"
+                    value="default"
+                    checked={selectedSort === "default"}
+                    onChange={handleChange}
+                    className="form-check-input"
+                    />
+                    Default
+                </label>
+                </div>
+
+                <div className="form-check">
+                <label>
+                    <input
+                    type="radio"
+                    value="az"
+                    checked={selectedSort === "az"}
+                    onChange={handleChange}
+                    className="form-check-input"
+                    />
+                    A-Z
+                </label>
+                </div>
+
+                <div className="form-check">
+                <label>
+                    <input
+                    type="radio"
+                    value="created"
+                    checked={selectedSort === "created"}
+                    onChange={handleChange}
+                    className="form-check-input"
+                    />
+                    Created date
+                </label>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Sort
