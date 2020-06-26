2d548cbae02a4b11453dab4d6c5068c4b5747bc0
gitburd <denaburd@gmail.com>
2020-06-26 10:26:14 -0700

    display all data

--- src/components/AlgorithmItem.js
+++ src/components/AlgorithmItem.js
@@ -1,13 +1,50 @@
 import React from 'react'
 
 const AlgorithmItem = ({algorithm}) => {
-    const {name} = algorithm;
+    const {name, imageUrl, pros, cons, averagePerformance, addedOn, description, moreDetailsUrl} = algorithm;
 
     return (
         <div>
-            {name && (
-                <h2>{name}</h2>
-            )}        
+            {name && <h2>{name}</h2>}        
+            {imageUrl && 
+            <img 
+                src={imageUrl}
+                alt={name}
+                style={{
+                    width:'200px'
+                }}
+            />}
+
+            {description && <p>{description}</p>}
+            
+            <h3>Big O Notation</h3>
+            {averagePerformance && <p>{averagePerformance}</p>}
+            
+            <h3>pros</h3>
+            {pros && pros.length > 0 && (
+                <ul>
+                    {pros.map((pro,idx) => (
+                        <li key={idx} >{pro}</li>
+                    ))}
+                </ul>
+            )}
+
+            <h3>cons</h3>
+            {cons && cons.length > 0 && (
+                <ul>
+                    {cons.map((con,idx) => (
+                        <li key={idx} >{con}</li>
+                    ))}
+                </ul>
+            )}
+
+            {addedOn && <p>Added: {addedOn}</p>}
+        
+            
+            {moreDetailsUrl && (
+                <a href={moreDetailsUrl} target='_blank'> MORE DETAILS</a>
+            )}
+            
         </div>
     )
 }
